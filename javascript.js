c741471574b2ad402fa0c58e6e2510c694c92cf1
Liemmjo <140917436+Liemmjo24@users.noreply.github.com>
2023-12-20 15:45:42 -0500

    Added comments

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
 let playerScore;
 let computerScore;
 
+// generate random between rock paper scissors
 function getComputerChoice(){
-    // generate random between rock paper scissors
     let words = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() *words.length);
     return words[randomIndex];
 }
 
-
+// determines whether the player or computer won
 function playRound(playerSelection, computerSelection) {
+
+    // if both used the same hand, its a tie
     if (playerSelection === computerSelection) {
         return ('It\'s a tie!');
     }
+    
+    // if the player won, adds a point to player
     if(
         (playerSelection === 'rock' && computerSelection === 'scissors'),
         (playerSelection === 'scissors' && computerSelection === 'paper'),
@@ -21,6 +25,8 @@ function playRound(playerSelection, computerSelection) {
         playerScore++;
         return('You win!');
     }
+
+    //if the computer won, add a point to computer
     if(
         (computerSelection === 'rock' && playerSelection === 'scissors'),
         (computerSelection === 'scissors' && playerSelection === 'paper'),
@@ -31,22 +37,26 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+// plays the game
 function game(playerChoice){
     console.log("Best of 5 game!");
+
+    // case-insensitive
     playerChoice = playerChoice.toLowerCase();
     let computerChoice = getComputerChoice();
     console.log('Computer chose ' + computerChoice);
     console.log(playRound(playerChoice, computerChoice));
 
+    // shows the player won
     if(playRound(playerChoice, computerChoice) === 'You'){
         alert("Player won the round!");
-        playerScore++;
     }
+    // shows the computer won
     else if (playRound(playerChoice, computerChoice) ==='Computer'){
         alert('Computer won the round');
-        computerScore++;
     }
 
+    // 
     if(playerScore === 5 || computerScore === 5){
         if(playerScore > computerScore){
             console.log('Player won best of five!');
@@ -73,14 +83,17 @@ function game(playerChoice){
         alert('Thanks for playing!');
     }
 }
-
+// start of the game
 function bestOfFive(){
     playerScore = 0;
     computerScore = 0;
+    
+    //allows the player to pick between rock paper scissors
     let playerChoice = prompt('rock, paper, scissors');
     game(playerChoice);
 }
 
+//shows the score at the end
 function showScores(){
     alert(`Player score: ${playerScore}\n Computer Score:${computerScore}`);
 }
