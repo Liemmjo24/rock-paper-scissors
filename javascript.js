9a6f6f7b692a5f3a946da2d329ec35224fce941f
Liemmjo <140917436+Liemmjo24@users.noreply.github.com>
2023-12-24 10:55:01 -0500

    Changed everything
    Added the GUI (buttons)

--- javascript.js
+++ javascript.js
@@ -1,102 +1,68 @@
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = "It's a tie!";
+const buttons = document.querySelectorAll('input');
 
+
+function disableButtons(){
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 // generate random between rock paper scissors
 function getComputerChoice(){
     let words = ['rock', 'paper', 'scissors'];
-    let randomIndex = Math.floor(Math.random() *words.length);
+    let randomIndex = Math.floor(Math.random() * words.length);
     return words[randomIndex];
 }
 
+
 // determines whether the player or computer won
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = ""
 
     // if both used the same hand, its a tie
     if (playerSelection === computerSelection) {
-        return ('It\'s a tie!');
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
-    
     // if the player won, adds a point to player
     if(
-        (playerSelection === 'rock' && computerSelection === 'scissors'),
-        (playerSelection === 'scissors' && computerSelection === 'paper'),
-        (playerSelection === 'paper' && computerSelection === 'rock')
+        (playerSelection == 'rock' && computerSelection == 'scissors'),
+        (playerSelection == 'scissors' && computerSelection == 'paper'),
+        (playerSelection == 'paper' && computerSelection == 'rock')
     ){
-        playerScore++;
-        return('You win!');
+        playerScore++
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
 
+    if (playerScore === 5){
+        result += 'You won the game!'
+        disableButtons()
+    }
     //if the computer won, add a point to computer
     if(
-        (computerSelection === 'rock' && playerSelection === 'scissors'),
-        (computerSelection === 'scissors' && playerSelection === 'paper'),
-        (computerSelection === 'paper' && playerSelection === 'rock')
+        (computerSelection == 'rock' && playerSelection == 'scissors'),
+        (computerSelection == 'scissors' && playerSelection == 'paper'),
+        (computerSelection == 'paper' && playerSelection == 'rock')
     ){
         computerScore++
-        return('Computer win!')
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+        + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
+    if (computerScore === 5)
+    {
+        result += '<br><br>I won the game! Reload the page to play again'
+        disableButtons()
+    }
+    document.getElementById('result').innerHTML = result
+    return
 }
 
-// plays the game
-function game(playerChoice){
-    console.log("Best of 5 game!");
-
-    // case-insensitive
-    playerChoice = playerChoice.toLowerCase();
-    let computerChoice = getComputerChoice();
-    console.log('Computer chose ' + computerChoice);
-    console.log(playRound(playerChoice, computerChoice));
-
-    // shows the player won
-    if(playRound(playerChoice, computerChoice) === 'You'){
-        alert("Player won the round!");
-    }
-    // shows the computer won
-    else if (playRound(playerChoice, computerChoice) ==='Computer'){
-        alert('Computer won the round');
-    }
-
-    // 
-    if(playerScore === 5 || computerScore === 5){
-        if(playerScore > computerScore){
-            console.log('Player won best of five!');
-            alert('Player won best of five!')
-        }
-        else if (computerChoice > playerScore)
-        {
-            console.log('Computer won the best of five!');
-            alert('Computer won best of Five!')
-        }
-        else{
-            console.log('It\'s a tie!');
-            alert('It\'s a tie!');
-        }
-        return;
-    }
-    
-    let playAgain = prompt('Want to try again? (yes or no)').toLowerCase();
-    if (playAgain === 'yes'){
-        let newUserChoice = prompt('rock, paper, scissors');
-        game(newUserChoice);
-    }
-    else{
-        alert('Thanks for playing!');
-    }
-}
-// start of the game
-function bestOfFive(){
-    playerScore = 0;
-    computerScore = 0;
-    
-    //allows the player to pick between rock paper scissors
-    let playerChoice = prompt('rock, paper, scissors');
-    game(playerChoice);
-}
-
-//shows the score at the end
-function showScores(){
-    alert(`Player score: ${playerScore}\n Computer Score:${computerScore}`);
-}
-
-bestOfFive();
-showScores();
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
